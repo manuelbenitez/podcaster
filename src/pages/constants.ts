d1916a58d51cf27d5dbc1ce255c9e870b56a6b94
Manuel <manu.benitez1@hotmail.com>
2023-04-01 15:13:27 +0200

    feature: podcast details: basic setup

--- src/pages/constants.ts
+++ src/pages/constants.ts
@@ -1,1 +1,5 @@
-export const TOP_100_ITUNES_PODCASTS_URL = "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json";
+export const TOP_100_ITUNES_PODCASTS_URL =
+  "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json";
+
+export const API_ORIGINS_URL =
+  "https://api.allorigins.win/get?url=";
---
+++ src/pages/podcast/PodcastDetailsPage.types.d.ts
@@ -0,0 +1,5 @@
+export interface IEpisodes {
+  trackName: string;
+  releaseDate: Date;
+  trackTimeMillis: number;
+}

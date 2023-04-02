bed52c2996ad569c69c2f1a98046e232f3cfafd3
Manuel <manu.benitez1@hotmail.com>
2023-04-02 14:02:07 +0200

    feature: podcast filter

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Filter.module.scss";
+import { IFilterProps } from "./Filter.types";
+function Filter({ onChange, podcastCount, value }: IFilterProps) {
+  return (
+    <div className={styles.filterContainer}>
+      {podcastCount}
+      <input
+        placeholder="Filter podcasts..."
+        value={value}
+        onChange={(e) => onChange(e.currentTarget.value)}
+      />
+    </div>
+  );
+}
+
+export default Filter;
--- src/components/PodcastCard/PodcastCard.tsx
+++ src/components/PodcastCard/PodcastCard.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { IPodcastCard } from "./PodcastCard.types";
 import styles from "./PodcastCard.module.scss";
 import { useRouter } from "next/router";
+import Filter from "../Filter/Filter";
 
 const PodcastCards = ({ feed }: IPodcastCard) => {
   const { entry } = feed;
   const router = useRouter();
+
+  const [value, setValue] = useState<string>("");
+  const [podcastCount, setPodcastCount] = useState<number>(0);
 
   const handleClick = (id: string, summary: string) => {
     router.push(
@@ -15,27 +19,51 @@ const PodcastCards = ({ feed }: IPodcastCard) => {
     );
   };
   return (
-    <div className={styles.container}>
-      {entry &&
-        entry.map((podcast, index) => (
-          <div
-            key={index}
-            className={styles.podcastCard}
-            onClick={() =>
-              handleClick(podcast.id.attributes["im:id"], podcast.summary.label)
+    <div className={styles.page}>
+      <div className={styles.filterContainer}>
+        <Filter
+          podcastCount={podcastCount ? podcastCount : 100}
+          onChange={setValue}
+          value={value}
+        />
+      </div>
+      <div className={styles.container}>
+        {entry &&
+          entry.map((podcast, index) => {
+            if (
+              (value &&
+                podcast["im:artist"].label
+                  .toLowerCase()
+                  .includes(value.toLowerCase())) ||
+              podcast["im:name"].label
+                .toLowerCase()
+                .includes(value.toLowerCase())
+            ) {
+              return (
+                <div
+                  key={index}
+                  className={styles.podcastCard}
+                  onClick={() =>
+                    handleClick(
+                      podcast.id.attributes["im:id"],
+                      podcast.summary.label
+                    )
+                  }
+                >
+                  <Image
+                    src={podcast["im:image"][0].label}
+                    alt=""
+                    width={100}
+                    height={100}
+                    className={styles.image}
+                  />
+                  <h1>{podcast["im:name"].label}</h1>
+                  <p>Author: {podcast["im:artist"].label}</p>
+                </div>
+              );
             }
-          >
-            <Image
-              src={podcast["im:image"][0].label}
-              alt=""
-              width={100}
-              height={100}
-              className={styles.image}
-            />
-            <h1>{podcast["im:name"].label}</h1>
-            <p>Author: {podcast["im:artist"].label}</p>
-          </div>
-        ))}
+          })}
+      </div>
     </div>
   );
 };
